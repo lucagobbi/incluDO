0d8959e82e703809aa01b4d7fb66da7273aa91ff
lucagobbi <lucasghegobbi@gmail.com>
2023-02-11 10:06:51 +0100

    tailwind & auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
-import {Auth, onAuthStateChanged, User} from "@angular/fire/auth";
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  User
+} from "@angular/fire/auth";
+import {Router} from "@angular/router";
+import {addDoc, collection, doc, Firestore, serverTimestamp, setDoc} from "@angular/fire/firestore";
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +16,36 @@ export class AuthService {
 
   currentUser: User | null | undefined;
 
-  ui: any;
-
-  constructor(public auth: Auth) {
+  constructor(
+    public auth: Auth,
+    private router: Router,
+    private db: Firestore,
+  ) {
     onAuthStateChanged(this.auth, user => {
       this.currentUser = user;
     });
   }
+
+  signInWithEmailAndPassword(credentials: any) {
+    signInWithEmailAndPassword(this.auth, credentials.email, credentials.password)
+      .then(() => {
+      this.router.navigateByUrl('/dashboard');
+    });
+  }
+
+  signUp(credentials: any) {
+    createUserWithEmailAndPassword(this.auth, credentials.email, credentials.password)
+      .then(async (user: any) => {
+        const userDoc = doc(this.db, `users/${user.uid}`);
+        await setDoc(userDoc, {
+          name: credentials.name,
+          email: credentials.email,
+          created: serverTimestamp()
+        });
+        this.router.navigateByUrl('/dashboard');
+      });
+  }
+
+
 
 }
